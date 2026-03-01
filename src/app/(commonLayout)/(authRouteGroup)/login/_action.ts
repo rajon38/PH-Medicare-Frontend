2e812e8e1036f8eefae0751dacf51c8929ac2d97
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-03-01 21:44:21 +0600

    add login action, validation schema, and response types for user authentication

---
+++ src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use server'
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse } from "@/types/api.types";
+import { ILoginResponse } from "@/types/auth.types";
+import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
+
+export const loginAction = async (payload: ILoginPayload) : Promise<ILoginResponse | ApiErrorResponse> => {
+    const parsedPayload = loginZodSchema.safeParse(payload);
+
+    if (!parsedPayload.success) {
+        const firstError = parsedPayload.error.issues[0].message;
+        return {
+            success: false,
+            message: firstError
+        }
+    }
+    try {
+        const response = await httpClient.post<ILoginResponse>('/auth/login', parsedPayload.data);
+        const { accessToken, refreshToken, token, user } = response.data;
+        return response.data;
+    } catch (error: any) {
+        return {
+            success: false,
+            message: `Login failed: ${error.message}`
+        }
+    }
+}
