28045d7b777e08f6a68bfcc7aeeb3ec70de51b19
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-03-03 13:05:35 +0600

    add token management and cookie utilities for authentication

--- src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
+++ src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server'
 import { httpClient } from "@/lib/axios/httpClient";
+import { setTokenInCookies } from "@/lib/tokenUtils";
 import { ApiErrorResponse } from "@/types/api.types";
 import { ILoginResponse } from "@/types/auth.types";
 import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
+import { redirect } from "next/navigation";
 
 export const loginAction = async (payload: ILoginPayload) : Promise<ILoginResponse | ApiErrorResponse> => {
     const parsedPayload = loginZodSchema.safeParse(payload);
@@ -17,8 +19,11 @@ export const loginAction = async (payload: ILoginPayload) : Promise<ILoginRespon
     }
     try {
         const response = await httpClient.post<ILoginResponse>('/auth/login', parsedPayload.data);
-        const { accessToken, refreshToken, token, user } = response.data;
-        return response.data;
+        const { accessToken, refreshToken, token } = response.data;
+        await setTokenInCookies('accessToken', accessToken);
+        await setTokenInCookies('refreshToken', refreshToken);
+        await setTokenInCookies('better-auth.session_token', token);
+        redirect('/dashboard');
     } catch (error: any) {
         return {
             success: false,
