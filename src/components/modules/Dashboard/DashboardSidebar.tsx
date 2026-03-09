a999247667ae8a94747f332dfbe3ea4b7c8b4135
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-03-09 12:09:44 +0600

    add DashboardSidebar and DashboardSidebarContent components with navigation logic and user info display; implement nav items retrieval based on user role

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,4 +1,12 @@
-const DashboardSidebar = () => {
+import { getDefaultDashboardRoute } from "@/lib/authUtils";
+import { getNavItemsByRole } from "@/lib/navitems";
+import { getUserInfo } from "@/services/auth.services";
+import { NavSection } from "@/types/dashboard.types";
+
+const DashboardSidebar = async () => {
+    const userInfo = await getUserInfo();
+    const navItems : NavSection[] = getNavItemsByRole(userInfo.role);
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role);
     return (
         <div>
             Dashboard Sidebar
