81d88254bc77f0bfc2d5cf781c2ba69a228c98f3
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-03-11 15:57:15 +0600

    add recharts library and implement AppointmentBarChart and AppointmentPieChart components
    - Added recharts dependency to package.json and pnpm-lock.yaml
    - Created AppointmentBarChart component for visualizing appointment trends using a bar chart
    - Created AppointmentPieChart component for displaying appointment data in a pie chart format
    - Updated dashboard types to include PieChartData and BarChartData interfaces
    - Implemented StatsCard component for displaying statistics with icons and descriptions
    - Refactored DoctorList component to use more descriptive variable names for clarity

--- src/components/modules/Consultation/DoctorList.tsx
+++ src/components/modules/Consultation/DoctorList.tsx
@@ -4,7 +4,7 @@ import { getDoctors } from "@/app/(commonLayout)/consultation/_actions";
 import { useQuery } from "@tanstack/react-query";
 
 const DoctorsList = () => {
-    const { data } = useQuery({
+    const { data: doctorData } = useQuery({
     queryKey: ['doctors'],
     queryFn: () => getDoctors(),
   })
@@ -16,10 +16,10 @@ const DoctorsList = () => {
 
 //   console.log(nonPrefactchedData);
 
-  console.log(data);
+  console.log(doctorData);
     return (
         <div>
-            {data?.data?.map((doctor: any) => (
+            {doctorData?.data?.map((doctor: any) => (
                 <div key={doctor.id}>
                     <h2>{doctor.name}</h2>
                 </div>
--- src/types/dashboard.types.ts
+++ src/types/dashboard.types.ts
@@ -8,3 +8,14 @@ export interface NavSection {
     title ?: string,
     items : NavItem[]
 }
+
+export interface PieChartData {
+    status: string,
+    count: number
+}
+
+export interface BarChartData {
+    month: Date | string;
+    count: number;
+}
+
