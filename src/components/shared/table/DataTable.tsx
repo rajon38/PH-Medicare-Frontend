8f80e470837494eaa86de5cbd1cf2654409953a4
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-03-15 13:39:12 +0600

    refactor DataTable component; implement action buttons for view, edit, and delete using DropdownMenu

--- src/components/shared/table/DataTable.tsx
+++ src/components/shared/table/DataTable.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ColumnDef, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
 
 interface DataTableActions <TData>{
     onView?: (data: TData) => void;
@@ -16,9 +19,55 @@ interface DataTableProps <TData> {
 
 const DataTable = <TData,>({ data, columns, actions, emptyMessage, isLoading }: DataTableProps<TData>) => {
     
+    const tableColumns : ColumnDef<TData>[] = actions ? [
+        ...columns,
+        {
+            id: 'actions',
+            header: 'Actions',
+            cell: ({ row }) => {
+                const rowData = row.original;
+
+                return (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent align="end">
+                            {
+                                actions.onView && (
+                                    <DropdownMenuItem onClick={()=> actions.onView?.(rowData)}>
+                                        View
+                                    </DropdownMenuItem>
+                                )
+                            }
+                            {
+                                actions.onEdit && (
+                                    <DropdownMenuItem onClick={()=> actions.onEdit?.(rowData)}>
+                                        Edit
+                                    </DropdownMenuItem>
+                                )
+                            }
+                            {
+                                actions.onDelete && (
+                                    <DropdownMenuItem onClick={()=> actions.onDelete?.(rowData)}>
+                                        Delete
+                                    </DropdownMenuItem>
+                                )
+                            }
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )
+            }
+        }
+    ] : columns;
+
     const table = useReactTable({
         data,
-        columns,
+        columns: tableColumns,
         getCoreRowModel: getCoreRowModel()
     })
     return (
